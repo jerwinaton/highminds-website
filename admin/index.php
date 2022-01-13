390a8f7f024fd7966f96a91330043521f19c4104
Jerwin Mathew Aton <jerwin.mathew@gmail.com>
2022-01-13 16:29:15 +0800

    Feat: add mockup data retrieval for categories

--- admin/index.php
+++ admin/index.php
@@ -128,82 +128,55 @@
     </section>
     <section>
       <div class="outputs">
-        <div class="row justify-content-center">
-          <div
-            class="col-12 col-sm-6 col-md-4 col-lg-3 my-card-category-container"
-          >
-            <div class="category-actions">
-              <button class=""><i class="fas fa-edit fa-lg"></i></button>
-              <button class=""><i class="fas fa-trash-alt fa-lg"></i></button>
-            </div>
-            <div class="my-card my-card-category">
-              <div class="my-card-head my-card-category-head">
-                <h3>Products</h3>
-              </div>
-              <div class="my-card-body my-card-category-body">
-                <i class="fas fa-tshirt fa-3x"></i>
-                <h1>0</h1>
-              </div>
-              <div class="my-card-footer my-card-category-footer">
-                View All <i class="fas fa-chevron-right"></i>
-              </div>
-            </div>
-          </div>
-
-          <div
-            class="col-12 col-sm-6 col-md-4 col-lg-3 my-card-category-container"
-          >
-            <div class="category-actions">
-              <button class=""><i class="fas fa-edit fa-lg"></i></button>
-              <button class=""><i class="fas fa-trash-alt fa-lg"></i></button>
-            </div>
-            <div class="my-card my-card-category">
-              <div class="my-card-head my-card-category-head">
-                <h3>Products</h3>
-              </div>
-              <div class="my-card-body my-card-category-body">
-                <i class="fas fa-tshirt fa-3x"></i>
-                <h1>0</h1>
-              </div>
-              <div class="my-card-footer my-card-category-footer">
-                View All <i class="fas fa-chevron-right"></i>
-              </div>
-            </div>
-          </div>
-
-          <div
-            class="col-12 col-sm-6 col-md-4 col-lg-3 my-card-category-container"
-          >
-            <div class="category-actions">
-              <button class=""><i class="fas fa-edit fa-lg"></i></button>
-              <button class=""><i class="fas fa-trash-alt fa-lg"></i></button>
-            </div>
-            <div class="my-card my-card-category">
-              <div class="my-card-head my-card-category-head">
-                <h3>Products</h3>
-              </div>
-              <div class="my-card-body my-card-category-body">
-                <i class="fas fa-tshirt fa-3x"></i>
-                <h1>0</h1>
-              </div>
-              <div class="my-card-footer my-card-category-footer">
-                View All <i class="fas fa-chevron-right"></i>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div class="row justify-content-center"></div>
       </div>
     </section>
     <!-- end of outputs section -->
+
     <script>
       $(document).ready(() => {
-        // show action buttons
-        $(".my-card-category-container").on("mouseover", (e) => {
-          $(e.currentTarget).find(".category-actions").css("right", "0px");
-        });
-        // hide action buttons
-        $(".my-card-category-container").on("mouseleave", (e) => {
-          $(e.currentTarget).find(".category-actions").css("right", "-120px");
+        $(".categories").on("click", () => {
+          // global variable , container for all content
+          let allContent = "";
+          // if not yet active
+          if ($(".categories").not("active")) {
+            // sample retrieval of data and make cards
+            const container = $(".outputs").find(".row");
+            categories.forEach((result) => {
+              const content = `
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 my-card-category-container" >
+            <div class="category-actions">
+              <button class=""><i class="fas fa-edit fa-lg"></i></button>
+              <button class=""><i class="fas fa-trash-alt fa-lg"></i></button>
+            </div>
+            <div class="my-card my-card-category">
+              <div class="my-card-head my-card-category-head">
+                <h3>${result.name}</h3>
+              </div>
+              <div class="my-card-body my-card-category-body">
+                <img src="${result.image}" width=250>
+                <!-- <i class="fas fa-tshirt fa-3x"></i> -->
+                <h1>${result.total_products}</h1>
+              </div>
+              <div class="my-card-footer my-card-category-footer">
+                View All <i class="fas fa-chevron-right"></i>
+              </div>
+            </div>
+            </div>
+          `;
+              allContent += content;
+            });
+            container.html(allContent);
+          }
+
+          // show action buttons
+          $(".my-card-category-container").on("mouseover", (e) => {
+            $(e.currentTarget).find(".category-actions").css("right", "0px");
+          });
+          // hide action buttons
+          $(".my-card-category-container").on("mouseleave", (e) => {
+            $(e.currentTarget).find(".category-actions").css("right", "-120px");
+          });
         });
       });
     </script>
